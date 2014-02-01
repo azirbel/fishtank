051f3cd4f2220f5848ee1ed994d8b09c83b87cd3
Alex Zirbel <alex.zirbel@addepar.com>
2014-02-01 13:50:00 -0800

    Added bower/grunt and drew a circle in EaselJS

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'src/<%= pkg.name %>.js',
+        dest: 'build/<%= pkg.name %>.min.js'
+      }
+    },
+    watch: {
+      options: {
+        interrupt: true
+      },
+      code: {
+        files: [ '**/*.js' ],
+        tasks: [ 'uglify' ]
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify']);
+
+};
